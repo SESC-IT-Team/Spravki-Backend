3ea079f853e2b507ea010759db63bb61dd7ef01a
misha <msp666777999@gmail.com>
2026-05-15 11:47:50 +0500

    bug fix

--- src/schemas/order_shema.py
+++ src/schemas/order_shema.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 from uuid import UUID
-
+from typing import Optional
 from pydantic import BaseModel, ConfigDict
 
 
 class OrderShema(BaseModel):
     id: UUID
     number: int
-    link: str
+    link: Optional[str] = None
     full_name: str
     department: str
     certificate_type: str
